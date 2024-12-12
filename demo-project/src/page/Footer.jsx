3bac4b2ec4d407715b58d2322ef952770228a229
sajedul-hoque-shakil <shajedol01@gmail.com>
2024-12-12 19:26:46 +0600

    Update Footer.jsx

--- demo-project/src/page/Footer.jsx
+++ demo-project/src/page/Footer.jsx
@@ -4,6 +4,7 @@ export default function Foter(){
 <footer className="w-full h-20 bg-black text-white ">
    <h1 className="text-center text-3xl font-black">Developer By</h1>
    <h1 className="text-center text-4xl font-black">Sajedul Hoque Shakil</h1>
+    <p>h</p>
 </footer>
 </>
     );
